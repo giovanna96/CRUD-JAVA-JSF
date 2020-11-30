46450590b5434cbfd1edd86b18f83a1f33f4f58c
Giovanna <giovanna-severo@hotmail.com>
2020-11-30 10:49:59 -0300

    alterações

--- listacomprasjsf/src/main/java/br/com/desafio/bean/ProdutoMBean.java
+++ listacomprasjsf/src/main/java/br/com/desafio/bean/ProdutoMBean.java
@@ -1,20 +1,29 @@
 package br.com.desafio.bean;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.SessionScoped;
 
 import br.com.desafio.dao.DaoGeneric;
 import br.com.desafio.entidade.Produto;
 
 
 @ManagedBean
-@ViewScoped
+@SessionScoped
 public class ProdutoMBean {
 	
-	private Produto produto = new Produto();
+	private Produto produto;
+	
 	
 	private DaoGeneric<Produto> daoGeneric = new DaoGeneric<Produto>();
 	
+	
+	public ProdutoMBean() {
+		produto = new Produto();
+	}
+
 	public String salvar() {
 		daoGeneric.salvar(produto);
 		return "";
--- listacomprasjsf/src/main/java/br/com/desafio/entidade/Produto.java
+++ listacomprasjsf/src/main/java/br/com/desafio/entidade/Produto.java
@@ -7,8 +7,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="produto", schema = "compras")
 public class Produto implements Serializable {
 	
 	private static final long serialVersionUID = -549493438877551973L;
